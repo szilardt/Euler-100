1c093e94a757b5ab42dc20fcb2c9574929b70b87
SzilardTerdik <tszilard@gmail.com>
2020-04-17 22:01:00 +0100

    is_prime ready

--- Euler-003.py
+++ Euler-003.py
@@ -5,20 +5,16 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 import numpy as np
-
+    
 def is_prime(num):
-    i=3
+    assert (1<num)
+    
     max_num=np.floor(np.sqrt(num) ) + 1
-    print(str(num))
-    print(str(max_num))
+    
     while i< max_num:
         if 0==num%i  :
             return False
+        i+=1
     return True
 
-def find_primes(last_prime=2,to_max):
-    upper= np.sqrt(to_max)
-    while last_prime < upper:
-        if (0==to_max%last_prime): return last_prime
-        else:
-                last_prime += 1
+
