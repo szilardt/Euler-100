7cdb68f4d3287fd05c14716d5d70fba489465cea
SzilardTerdik <tszilard@gmail.com>
2020-05-26 12:12:14 +0100

    done

--- Euler-003.py
+++ Euler-003.py
@@ -6,12 +6,15 @@ What is the largest prime factor of the number 600851475143 ?
 
 import numpy as np
 
-num = input("Please enter your number: ")    
-num = int(num) # this will block anything which is not integer
 
-while num<2:
-    num = input("Please enter your number: ")    
-    num = int(num)
+checkbit= False# this will block anything which is not integer
+
+while not(checkbit):
+    try:
+        num = input("Please enter your number: ")    
+        num = int(num)
+        checkbit= True
+    except : print('Please enter a real integer higher than 1!')
 
 
 
@@ -32,30 +35,31 @@ def is_positive_prime(num):
 def list_primes(num):
     assert (np.floor(num) == num or 2>num)
     
-    
-    primes_list=[]
+    primes_max=0
     factors_list=[]
     
-    max_num= np.floor(num /2)+1
-    i=2
-    while i< max_num:
+    i=int(np.floor(num /2))
+    while i> 1:
         if 0==num %i : 
-            if is_positive_prime(i): primes_list.append(i)
+            if is_positive_prime(i):                 
+                primes_max=(i)
+                i=2
             else: factors_list.append(i)
-        i+=1            
+        i-=1            
     
-    
-    if ( len(factors_list)==0 and len(primes_list)==0 ): 
+    if ( len(factors_list)==0 and primes_max==0 ): 
         print('Congrats! You just have found a real prime number!')
         print(str(num)+ ' is prime')
-    elif len(primes_list)==0 :
-        print('There are no prime factors of:'+ str(num))
-        print('The non-prime factors of ' + str(num) + ' are:')
-        print(factors_list)
+    
+    elif len(factors_list)==0: 
+        print('There are no non-prime factors between ' + str(num) + ' and '+ str(primes_max))
+        print('Highest prime factor of ' + str(num) + ' is:') 
+        print(primes_max)
+    
     else: 
-        print('Prime factors of ' + str(num) + ' are:') 
-        print(primes_list)
-        print('Non-prime factors of ' + str(num) + ' are:') 
+        print('Highest prime factor of ' + str(num) + ' is:') 
+        print(primes_max)
+        print('Non-prime factors between ' + str(num) + ' and '+ str(primes_max) + ' are:') 
         print(factors_list)
 
 
